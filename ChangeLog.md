e2188687926418fcff6fb0ac1d84247730005504
Benjamin Friedman <friedman.benjamin@gmail.com>
2020-08-15 13:55:42 -0700

    trying w/ stack

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for ConceptGraph
+
+## Unreleased changes
--- ConceptGraph/ConceptDependency.hs
+++ ConceptGraph/ConceptDependency.hs
@@ -3,17 +3,15 @@
 -- where one concept is dependent on another
 --
 
-module ConceptDependency (ConceptDependency(ConceptDependency),conceptdep) where
+module ConceptDependency (ConceptDependency) where
 
 -- | Concept dependency linking types of a -> a, by 'b'
-data ConceptDependency b a =
-  ConceptDependency b a a
-  deriving (Eq,Ord)
+type ConceptDependency b a = (b,a,a)
 
 
-instance (Show b, Show a) => Show (ConceptDependency b a) where
-  show (ConceptDependency x y z) = show x ++ ", " ++ show y ++ ", " ++ show z ++ "\n"
+--instance (Show b, Show a) => Show (ConceptDependency b a) where
+--  show (ConceptDependency x y z) = show x ++ ", " ++ show y ++ ", " ++ show z ++ "\n"
 
--- | Type constructor for a concept dependency between two concepts
-conceptdep :: (b,a,a) -> ConceptDependency b a
-conceptdep (dep,n1,n2) = ConceptDependency dep n1 n2
+-- | Type constructor for a dependency between two concepts
+--conceptdep :: (b,a,a) -> ConceptDependency b a
+--conceptdep (dep,n1,n2) = (dep,n1,n2)
